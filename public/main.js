0d5b3724ae6b1629628b496f8e9dbdfcd531dee8
majemajem <75851302+majemajem@users.noreply.github.com>
2024-07-24 18:31:47 -0500

    primera prueba con audios

--- public/main.js
+++ public/main.js
@@ -4,30 +4,44 @@ const audioContext = new AudioContext();
 const modalTrack = document.querySelectorAll("a-entity");
 const sceneEl = document.querySelector('a-scene');
 const arSystem = sceneEl.systems["mindar-image-system"];
+let tracks = [];
+let audios = [];
 
 function insertarData(arrayOfEntities, arrayOfData) {
     arrayOfEntities.forEach((entity, index) => {
         // console.log(arrayOfAudios[0].audios[index]);
-        entity.insertAdjacentHTML('beforeend', `<audio src="${arrayOfData[0].audios[index].src}" autoplay="false"></audio>`);
-        entity.insertAdjacentHTML('beforeend', `<a-text class="textito" hidden="true" value="${arrayOfData[0].texts[index].content}" geometry="primitive:plane" color="black" font="monoid"></a-text>`);
-        entity.addEventListener("targetFound", event => {
-            console.log("se logró");
-            //Aquí entra la parte de activar los audios
-        });
+        entity.insertAdjacentHTML('beforeend', `<audio src="${arrayOfData[0].audios[index].src}" loop="false"></audio>`);
+        entity.insertAdjacentHTML('beforeend', `<a-text class="textito" hidden="true" value="${arrayOfData[0].texts[index].content}" geometry="primitive:plane" color="white" font="monoid"></a-text>`);
         // console.log(arrayOfAudios[0].audios[index].src);
     });
-    let audios = document.querySelectorAll("audio");
+    audios = document.querySelectorAll("audio");
     console.log(audios);
+    audios.forEach((audio, index) => {
+        const track = audioContext.createMediaElementSource(audio);
+        tracks.push(track);
+        tracks[index].connect(audioContext.destination);
+    });
+    console.log(tracks);
     let textos = document.querySelectorAll("a-text");
     textos.forEach((texto, index) => {
         // texto.height = '20px';
         texto.width = '0.3';
-        texto.wrapCount = '80';
+        texto.wrapCount = '10';
         texto.zOffset = '0.5';
-        console.log(arrayOfEntities[index]);
+        // console.log(arrayOfEntities[index]);    
     });
-    console.log(textos);
-    // console.log(modalTrack);
+    // console.log(textos);
+    arrayOfEntities.forEach((entity, index) => {
+        console.log(audios[index]);
+        entity.addEventListener("targetFound", event => {
+            audios[index].play();
+            console.log("se logró");
+            //Aquí entra la parte de activar los audios
+        });
+        entity.addEventListener("targetLost", event => {
+            audios[index].pause();
+        })
+    })
 };
 
 function fetchJSONData() {
