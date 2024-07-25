3749d03e35a40247e9291862f2ae257d58612308
majemajem <75851302+majemajem@users.noreply.github.com>
2024-07-25 18:41:09 -0500

    Limpieza

--- public/main.js
+++ public/main.js
@@ -1,21 +1,16 @@
 const audioContext = new AudioContext();
-// const inicioAudio = document.querySelector('.control');
-// primero debo inicializar cada audio en su respectivo
 const modalTrack = document.querySelectorAll("a-entity.contenedor");
 const sceneEl = document.querySelector('a-scene');
 const arSystem = sceneEl.systems["mindar-image-system"];
 let tracks = [];
 let audios = [];
 let buttonsEnlace = [];
-// let buttonsCompra = [];
-// let buttonsDescarga = [];
 
 function insertarData(arrayOfEntities, arrayOfData) {
     arrayOfEntities.forEach((entity, index) => {
         entity.insertAdjacentHTML('beforeend', `<audio src="${arrayOfData[0].audios[index].src}" loop="false"></audio>`);
         // entity.insertAdjacentHTML('beforeend', `<a-text class="textito" hidden="true" value="${arrayOfData[0].texts[index].content}" geometry="primitive:plane; height:2;width:1.6" material="color:#2e2e2e" align="left" anchor="center" baseline="center" color="white" font="mozillavr" width="1.55" wrapCount="100" zOffset="0.1" position="0 -1 2" rotation="-40 0 0"></a-text>`);
         entity.insertAdjacentHTML('beforeend', `<a-plane position="0 -0.7 1.5" rotation="-30 0 0" width="1.5" height="1.5" material="src:${arrayOfData[0].texts[index].src}; transparent: true"></a-plane>`);
-        // entity.insertAdjacentHTML('beforeend', `<a-plane position="0 0 1.4" rotation="-30 0 0" width="2" height="2" color="#e0e0e0"></a-plane>`);
         if (arrayOfData[0].links[index].type == 'biblioteca') {
             entity.insertAdjacentHTML('beforeend', `<a-image class="clickable biblioteca" src="#biblioteca" position="1 0.3 1.5" height="0.15" width="0.6" animation="property: scale; to: 1.3 1.3 1.3; dur: 2000; easing: easeInOutQuad; loop: true; dir: alternate" material geometry></a-image>`);
         } else if (arrayOfData[0].links[index].type == 'compra') {
@@ -56,10 +51,8 @@ function activarLinks(botones, arrayOfData) {
         button.addEventListener('click', function (evt) {
             window.location.href = arrayOfData[0].links[index].href;
         });
-        console.log(arrayOfData[0].links[index].href);
     });
-    console.log(botones);
-}
+};
 
 function fetchJSONData() {
     fetch("./public/data/datos.json")
