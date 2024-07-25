8820345403a42f3421c7eb9a8071cec2198f4c20
majemajem <75851302+majemajem@users.noreply.github.com>
2024-07-24 19:48:20 -0500

    más texto

--- public/main.js
+++ public/main.js
@@ -11,7 +11,7 @@ function insertarData(arrayOfEntities, arrayOfData) {
     arrayOfEntities.forEach((entity, index) => {
         // console.log(arrayOfAudios[0].audios[index]);
         entity.insertAdjacentHTML('beforeend', `<audio src="${arrayOfData[0].audios[index].src}" loop="false"></audio>`);
-        entity.insertAdjacentHTML('beforeend', `<a-text class="textito" hidden="true" value="${arrayOfData[0].texts[index].content}" geometry="primitive:plane; height:2;width:1" material="color:#b7a9a9" color="black" font="monoid" width="0.6" wrapCount="100" zOffset="0.5" position="0 -1 2" rotation="0 -40 0"></a-text>`);
+        entity.insertAdjacentHTML('beforeend', `<a-text class="textito" hidden="true" value="${arrayOfData[0].texts[index].content}" geometry="primitive:plane; height:2;width:1" material="color:#2e2e2e" align="center" color="white" font="monoid" width="1" wrapCount="100" zOffset="0.1" position="0 -1 2" rotation="-40 0 0"></a-text>`);
         // console.log(arrayOfAudios[0].audios[index].src);
     });
     audios = document.querySelectorAll("audio");
